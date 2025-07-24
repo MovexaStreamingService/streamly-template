9a28b4934948e29576cec706bd87ceaea01f36d8
MovexaStreamingService <jayjayblack0824@gmail.com>
2025-07-24 09:37:55 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Edit these values to update your site
+const siteName = "Movexa Streaming";
+const featuredVideo = "https://player.vimeo.com/video/VIDEO_ID";
+const trendingVideos = [
+  "https://player.vimeo.com/video/VIDEO_ID1",
+  "https://player.vimeo.com/video/VIDEO_ID2"
+];
